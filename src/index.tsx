31f8c9e6dea39f18f27b0982379f7d99b00cdaf4
sanha <kgpaper@naver.com>
2022-07-06 11:45:50 +0900

    feat: 구조 변경 및 패키지 설치

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Router from './Router';
+import GlobalStyles from './styles/GlobalStyles';
+import './styles/antd.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-root.render(<App />);
+root.render(
+  <>
+    <GlobalStyles />
+    <Router />
+  </>,
+);
---
+++ src/pages/Main.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Main(): JSX.Element {
+  return <div>main</div>;
+}
+
+export default Main;
